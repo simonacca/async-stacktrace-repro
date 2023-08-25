86ca084eff00f55dfff5035c2a985e0ac2279b94
Simon Accascina <simon@accascina.me>
2023-08-25 20:30:25 +0200

    Repro

--- src/main/starter/browser.cljs
+++ src/main/starter/browser.cljs
@@ -1,4 +1,5 @@
-(ns starter.browser)
+(ns starter.browser
+  (:require [shadow.cljs.modern :refer-macros [js-await]]))
 
 ;; start is called by init and after code reloading finishes
 (defn ^:dev/after-load start []
@@ -14,3 +15,17 @@
 ;; this is called before any code is reloaded
 (defn ^:dev/before-load stop []
   (js/console.log "stop"))
+
+
+
+(defn my-parse-fn [res]
+  (js-await [body (.json res)]
+            (js/console.log "got some json" body)))
+
+(defn my-fetch-fn []
+  (js-await [res (js/fetch "/some.json")]
+            (js/console.log "res" res)
+            (js-await [parsed (my-parse-fn res)]
+                      (js/console.log parsed))))
+
+(js/console.log "my-fetch-fn" (my-fetch-fn))
